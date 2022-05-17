1e6aff9f07f5ff741b4419bd693337a95bbd7acf
csantacreu <carlossantacreuch@gmail.com>
2022-05-17 19:06:41 +0200

    beautifulSoup gets prices from html

--- dnmlp.py
+++ dnmlp.py
@@ -1,23 +1,36 @@
 
-# DarkNet Market Listing Parser
+# /////////////// DarkNet Market Listing Parser ///////////////
 
 import os
+from bs4 import BeautifulSoup
 
 listings_path = './listings'
 html_files = []
 
 # os.chdir('./listings')
 
-print(f'cd:     {os.getcwd()}')
+print(f'\nCD:     {os.getcwd()}\n')
 
 for (dirpath, dirname, filenames) in os.walk(listings_path):
     i = 0
     for file_name in filenames:
         i += 1
-        print(str(i) + ' ' + file_name)
         # print(f'{i} {file_name}')6
-        # Stores the absolute path of each c and h file in file_path
         file_path = os.path.join(os.path.abspath(dirpath), file_name)
         html_files.append(file_path)
-print(html_files)
+
 # funcion para cribar cada archivo según la sustancia que sea
+def which_market():
+    pass
+
+i = 0
+for file in html_files:
+    i += 1
+    with open(file) as fl:
+        soup = BeautifulSoup(fl, 'html.parser')
+        # print(f'Listing title:  {str(soup.body.h4.text)}')
+        label = soup.find('th', {'class': 'cell-label'})
+        label_price = label.parent.td.text
+        print(label_price)
+        # print(f'{i}:    {label_price.td.text}')
+        
