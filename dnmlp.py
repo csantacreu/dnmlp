cab27458b45889ce17bc7c1364960a7e66792431
csantacreu <carlossantacreuch@gmail.com>
2022-05-24 23:02:18 +0200

    added url and other fields

--- dnmlp.py
+++ dnmlp.py
@@ -2,25 +2,25 @@
 import os
 import re
 from openpyxl import load_workbook
-from bs4 import BeautifulSoup
 from lxml import html
+from datetime import datetime
 
 EXCEL_FILE = 'data.xlsx'
 LISTINGS_PATH = './listings'
-MARKET_COL = 'A'
-DATE_COL = 'B'
-SUBNAME_COL = 'C'
-VENDOR_COL = 'D'
-URL_COL = 'E'
-TITLE_COL = 'F'
-QUANTITY_COL = 'G'
-PRICE_COL = 'H'
-TRANS_COL = 'I'
-S_FROM_COL = 'J'
-S_TO_COL = 'K'
-DESCRIPTION_COL = 'L'
-
-# print(f'\nCD:     {os.getcwd()}\n')
+COLUMN = {
+    'MARKET_COL': 'A',
+    'DATE_COL': 'B',
+    'SUBNAME_COL': 'C',
+    'VENDOR_COL': 'D',
+    'URL_COL': 'E',
+    'TITLE_COL': 'F',
+    'QUANTITY_COL': 'G',
+    'PRICE_COL': 'H',
+    'TRANS_COL': 'I',
+    'S_FROM_COL': 'J',
+    'S_TO_COL': 'K',
+    'DESCRIPTION_COL': 'L'
+}
 
 html_files = []
 for (dirpath, dirname, filenames) in os.walk(LISTINGS_PATH):
@@ -31,12 +31,9 @@ for (dirpath, dirname, filenames) in os.walk(LISTINGS_PATH):
         file_path = os.path.join(os.path.abspath(dirpath), file_name)
         html_files.append(file_path)
 
-# funcion para cribar cada archivo según la sustancia que sea
-def which_market():
-    pass
-
 i = 0
 vendors = []
+urls = []
 titles = []
 prices = []
 raw_prices = []
@@ -45,11 +42,13 @@ s_tos = []
 descriptions = []
 listings = []
 for file in html_files:
-    print(f'File:   {file}')
     tree = html.parse(file)
-    root = tree.getroot()
     vendor = tree.xpath('//label[contains(text(),"Vendor:")]/../../td/a/text()')[0]
     vendors.append(vendor)
+    listing_reference = tree.xpath('//a[contains(text(),"Positive")]/@href')[0]
+    listing_reference = re.search(r'\/*.*\/', listing_reference)[0]
+    url = f'http://asap2u4pvplnkzl7ecle45wajojnftja45wvovl3jrvhangeyq67ziid.onion{listing_reference}'
+    urls.append(url)
     title = tree.xpath('/html/body/div/div[3]/div/div[1]/h4/text()')[0]
     titles.append(title)
     price = tree.xpath('//label[contains(text(), "Price:")]/../../td/text()')
@@ -57,11 +56,11 @@ for file in html_files:
         price = '!!!no price'
     else:
         price = price[0].translate({ord(i): None for i in '\n\r ,'})
-    if price.endswith('USD'):
-        price = re.search(r'\d+\.?\d*', price)[0]
-    elif not price.endswith('USD'):
-        label_currency = re.search(r'\w{3}', price)[0]
-    print(price)
+        if price.endswith('USD'):
+            price = re.search(r'\d+\.?\d*', price)[0] + ' USD'
+        else:
+            label_currency = re.search(r'\w{3}', price)[0]
+            price = f'({label_currency}) {price}'
     prices.append(price)
     s_from = tree.xpath('//label[contains(text(), "Ships from:")]/../../td/text()')[0]
     s_froms.append(s_from)
@@ -69,9 +68,9 @@ for file in html_files:
     s_tos.append(s_to)
     description = tree.xpath('//h5[contains(text(),"Description")]/../div/text()')[0]
     descriptions.append(description)
-    print(s_from, s_to)
     listing = {
             'vendor': vendor,
+            'url': url,
             'title': title,
             'price': price,
             's_from': s_from,
@@ -82,19 +81,11 @@ for file in html_files:
 
 workbook = load_workbook(filename=EXCEL_FILE)
 sheet = workbook.active
-row_n = 3
+row_n = 2
 for listing in listings:
-    title_coordinate = f'{TITLE_COL}{row_n}'
-    sheet[title_coordinate] = listing['title']
-    price_coordinate = f'{PRICE_COL}{row_n}'
-    sheet[price_coordinate] = listing['price']
-    vendor_coordinate = f'{VENDOR_COL}{row_n}'
-    sheet[vendor_coordinate] = listing['vendor']
-    s_from_coordinate = f'{S_FROM_COL}{row_n}'
-    sheet[s_from_coordinate] = listing['s_from']
-    s_to_coordinate = f'{S_TO_COL}{row_n}'
-    sheet[s_to_coordinate] = listing['s_to']
-    description_coordinate = f'{DESCRIPTION_COL}{row_n}'
-    sheet[description_coordinate] = listing['description']
     row_n += 1
+    for key, value in listing.items():
+        col = key.upper() + '_COL'
+        coordinate = f'{COLUMN[col]}{row_n}'
+        sheet[coordinate] = listing[key]
 workbook.save(filename=EXCEL_FILE)
