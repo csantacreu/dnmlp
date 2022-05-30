a91fb5bda75264a1fa55e6e60a5e9f8dfd2aabde
csantacreu <carlossantacreuch@gmail.com>
2022-05-30 17:57:40 +0200

    added market and date data

--- dnmlp.py
+++ dnmlp.py
@@ -5,6 +5,7 @@ from openpyxl import load_workbook
 from lxml import html
 from datetime import datetime
 
+DESTINATIONS = ('Worldwide', 'European Union', 'United Kingdom')
 EXCEL_FILE = 'data.xlsx'
 LISTINGS_PATH = './listings'
 COLUMN = {
@@ -22,6 +23,17 @@ COLUMN = {
     'DESCRIPTION_COL': 'L'
 }
 
+ASAP_XPATHS = {
+    'vendor': '//label[contains(text(),"Vendor:")]/../../td/a/text()',
+    # 'url': url,
+    # 'title': title,
+    # 'quantity': quantity,
+    # 'price': price,
+    # 's_from': s_from,
+    # 's_to': s_to,
+    # 'description': description
+}
+
 html_files = []
 for (dirpath, dirname, filenames) in os.walk(LISTINGS_PATH):
     i = 0
@@ -32,25 +44,30 @@ for (dirpath, dirname, filenames) in os.walk(LISTINGS_PATH):
         html_files.append(file_path)
 
 i = 0
-vendors = []
-urls = []
-titles = []
-prices = []
-raw_prices = []
-s_froms = []
-s_tos = []
-descriptions = []
 listings = []
 for file in html_files:
     tree = html.parse(file)
+
+    market = file.strip('.htm')
+
+    date = datetime.today().strftime('%d-%b-%Y')
+
     vendor = tree.xpath('//label[contains(text(),"Vendor:")]/../../td/a/text()')[0]
-    vendors.append(vendor)
-    listing_reference = tree.xpath('//a[contains(text(),"Positive")]/@href')[0]
-    listing_reference = re.search(r'\/*.*\/', listing_reference)[0]
-    url = f'http://asap2u4pvplnkzl7ecle45wajojnftja45wvovl3jrvhangeyq67ziid.onion{listing_reference}'
-    urls.append(url)
+    vendor = re.search(r'\w* ', vendor).group(0)
+    vendor = vendor[:-1]
+
+    reference = tree.xpath('//a[contains(text(),"Positive")]/@href')[0]
+    reference = re.search(r'\/*.*\/', reference)
+    url = f'http://asap2u4pvplnkzl7ecle45wajojnftja45wvovl3jrvhangeyq67ziid.onion{reference}'
+
     title = tree.xpath('/html/body/div/div[3]/div/div[1]/h4/text()')[0]
-    titles.append(title)
+
+    quantity = re.search(r'\d+\s*[gG]+', title)
+    if not quantity:
+        quantity = '!!!no quantity'
+    else:
+        quantity = re.search(r'\d+', quantity[0])[0] + 'g'
+
     price = tree.xpath('//label[contains(text(), "Price:")]/../../td/text()')
     if not price:
         price = '!!!no price'
@@ -61,26 +78,31 @@ for file in html_files:
         else:
             label_currency = re.search(r'\w{3}', price)[0]
             price = f'({label_currency}) {price}'
-    prices.append(price)
+
     s_from = tree.xpath('//label[contains(text(), "Ships from:")]/../../td/text()')[0]
-    s_froms.append(s_from)
     s_to = tree.xpath('//label[contains(text(), "Ships to:")]/../../td/text()')[0]
-    s_tos.append(s_to)
+
     description = tree.xpath('//h5[contains(text(),"Description")]/../div/text()')[0]
-    descriptions.append(description)
+
     listing = {
-            'vendor': vendor,
-            'url': url,
-            'title': title,
-            'price': price,
-            's_from': s_from,
-            's_to': s_to,
-            'description': description
+        'date': date,
+        'vendor': vendor,
+        'url': url,
+        'title': title,
+        'quantity': quantity,
+        'price': price,
+        's_from': s_from,
+        's_to': s_to,
+        'description': description
             }
+    print(title)
+    print(quantity)
+    print(vendor)
+    print(price + '\n')
     listings.append(listing)
 
-workbook = load_workbook(filename=EXCEL_FILE)
-sheet = workbook.active
+wb = load_workbook(filename=EXCEL_FILE)
+sheet = wb.active
 row_n = 2
 for listing in listings:
     row_n += 1
@@ -88,4 +110,4 @@ for listing in listings:
         col = key.upper() + '_COL'
         coordinate = f'{COLUMN[col]}{row_n}'
         sheet[coordinate] = listing[key]
-workbook.save(filename=EXCEL_FILE)
+wb.save(filename=EXCEL_FILE)
